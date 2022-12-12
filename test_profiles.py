112df7e5ba2b2b6d20ded4ae5581c1ef31a975f2
Alexander Smirnov <alexv-smirnov@yandex-team.ru>
2022-12-12 20:36:24 +0200

    fix1

--- test_profiles.py
+++ test_profiles.py
@@ -2,3 +2,4 @@ import ydb_profile
 
 def test_1():
     assert ydb_profile.getDriverPars( 'my1', {'profiles':{'my1':{'endpoint':'eee'}}}) == {'endpoint':'eee'}
+    assert ydb_profile.getDriverPars( 'my1', {'profiles':{'my1':{'database':'eee'}}}) == {'database':'eee'}
